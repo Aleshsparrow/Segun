68d2cbd5b4d4b3cf6bfcccc82decb24e89c62f0e
Segun Alesinloye <segunalesh@gmail.com>
2020-08-08 12:30:55 -0500

    Added animations to the project page

--- porfolio/src/Components/ProjectsComponent/Projects.jsx
+++ porfolio/src/Components/ProjectsComponent/Projects.jsx
@@ -8,11 +8,97 @@ class Projects extends Component {
     return (
       <Container>
         <div className="pro" className="col-sm-12">
+          <div className="pro-card delay1">
+            <div className="row no-gutters">
+              <div className="col-sm-6">
+                <img
+                  src="https://i.imgur.com/5sPU7RO.png"
+                  className="card-img"
+                  alt="..."
+                />
+              </div>
+              <div className="col-sm-6">
+                <div>
+                  <h2>Hero Box</h2>
+                  <p className="word">
+                    A subscription based e-commerce platform that allows user to
+                    subscribe to monthly delivery of comic books. I teamed up
+                    with four other developers on this project.
+                  </p>
+                  <p>
+                    <small>
+                      Technologies used: HTML, CSS, JavaScript, AJAX, API,
+                      React.js, Axios NPM package etc.
+                    </small>
+                  </p>
+                  {/* <br /> */}
+                  {/* <br /> */}
+                  <a href="https://i.imgur.com/5sPU7RO.png">
+                    <button>Link</button>
+                  </a>
+                  <a href="#">
+                    <button>Github</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* </a> */}
+          <br />
+          {/* <a
+  style="text-decoration: none;"
+  href="https://sheltered-shore-12233.herokuapp.com/"
+> */}
+
+          {/* <a
+            style="text-decoration: none;"
+            href="https://aleshsparrow.github.io/Weather-based-suggestions/"
+          > */}
+          <div className="pro-card delay2">
+            <div className="row no-gutters">
+              <div className="col-sm-6">
+                <img
+                  src="https://i.imgur.com/ELCTOYy.png"
+                  className="card-img"
+                  alt="..."
+                />
+              </div>
+              <div className="col-sm-6">
+                <div>
+                  <h2>Weather Based Suggestions</h2>
+                  <p className="word">
+                    A web app built for suggesting activities based on weather
+                    conditions. User is required to enter a city or zip code
+                    into the search area. Upon click the search button, the user
+                    will be supplied with some activities and some music to go
+                    with the mood. This app was built by me and one other
+                    developer
+                  </p>
+                  <p>
+                    <small>
+                      Technologies used: HTML, CSS, JavaScript, jQuery, AJAX,
+                      API etc.
+                    </small>
+                  </p>
+                  {/* <br /> */}
+                  <a href="https://aleshsparrow.github.io/Weather-based-suggestions/">
+                    <button>Link</button>
+                  </a>
+                  <a href="#">
+                    <button>Github</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* </a> */}
+          <br />
+
           {/* <a
             // style="text-decoration: none;"
             href="https://aleshsparrow.github.io/Weather-Dashboard/"
           > */}
-          <div className="delay" className="pro-card">
+          <div className="pro-card delay3">
             <div className="row no-gutters">
               <div className="col-sm-6">
                 <img
@@ -53,48 +139,7 @@ class Projects extends Component {
             style="text-decoration: none;"
             href="https://pacific-chamber-07789.herokuapp.com/"
           > */}
-          <div className="pro-card">
-            <div className="row no-gutters">
-              <div className="col-sm-6">
-                <img
-                  src="https://i.imgur.com/5sPU7RO.png"
-                  className="card-img"
-                  alt="..."
-                />
-              </div>
-              <div className="col-sm-6">
-                <div>
-                  <h2>Hero Box</h2>
-                  <p className="word">
-                    A subscription based e-commerce platform that allows user to
-                    subscribe to monthly delivery of comic books. I teamed up
-                    with four other developers on this project.
-                  </p>
-                  <p>
-                    <small>
-                      Technologies used: HTML, CSS, JavaScript, AJAX, API,
-                      React.js, Axios NPM package etc.
-                    </small>
-                  </p>
-                  {/* <br /> */}
-                  {/* <br /> */}
-                  <a href="https://i.imgur.com/5sPU7RO.png">
-                    <button>Link</button>
-                  </a>
-                  <a href="#">
-                    <button>Github</button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* </a> */}
-          <br />
-          {/* <a
-            style="text-decoration: none;"
-            href="https://sheltered-shore-12233.herokuapp.com/"
-          > */}
-          <div className="pro-card">
+          <div className="pro-card delay4">
             <div className="row no-gutters">
               <div className="col-sm-6">
                 <img
@@ -132,49 +177,7 @@ class Projects extends Component {
             </div>
           </div>
           {/* </a> */}
-          <br />
-          {/* <a
-            style="text-decoration: none;"
-            href="https://aleshsparrow.github.io/Weather-based-suggestions/"
-          > */}
-          <div className="pro-card">
-            <div className="row no-gutters">
-              <div className="col-sm-6">
-                <img
-                  src="https://i.imgur.com/ELCTOYy.png"
-                  className="card-img"
-                  alt="..."
-                />
-              </div>
-              <div className="col-sm-6">
-                <div>
-                  <h2>Weather Based Suggestions</h2>
-                  <p className="word">
-                    A web app built for suggesting activities based on weather
-                    conditions. User is required to enter a city or zip code
-                    into the search area. Upon click the search button, the user
-                    will be supplied with some activities and some music to go
-                    with the mood. This app was built by me and one other
-                    developer
-                  </p>
-                  <p>
-                    <small>
-                      Technologies used: HTML, CSS, JavaScript, jQuery, AJAX,
-                      API etc.
-                    </small>
-                  </p>
-                  {/* <br /> */}
-                  <a href="https://aleshsparrow.github.io/Weather-based-suggestions/">
-                    <button>Link</button>
-                  </a>
-                  <a href="#">
-                    <button>Github</button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* </a> */}
+
           <br />
           <br />
           <br />
