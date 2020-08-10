3f6f60846acd092fa3a652600903861c4bb99171
Segun Alesinloye <segunalesh@gmail.com>
2020-08-09 21:46:09 -0500

    Re-arranged the home page

--- porfolio/src/Components/HomeComponent/Home.jsx
+++ porfolio/src/Components/HomeComponent/Home.jsx
@@ -9,13 +9,13 @@ class Home extends Component {
     return (
       <Container>
         <Row>
-          <Col className="pic" xs={12} sm={6} md={6} lg={6} xl={6}>
+          <Col className="pic" xs={12} sm={12} md={4} lg={4} xl={4}>
             <img
               src="https://i.imgur.com/wsRpryK.jpg"
               title="Segun: imgur.com"
             />
           </Col>
-          <Col className="bio" xs={12} sm={6} md={6} lg={6} xl={6}>
+          <Col className="bio" xs={12} sm={12} md={8} lg={8} xl={8}>
             <div>
               <h1>Segun Alesinloye</h1>
               <h5>Chicago, 60618 | (872) 203-5491 | segunalesh@gmail.com</h5>
@@ -26,15 +26,14 @@ class Home extends Component {
               <h4>
                 My name is Segun Alesinloye, my pronouns are Him/His/He. I am a
                 full Stack Web Developer/Software Engineer with profiency in
-                technologies like{" "}
+                technologies like
                 <span>
                   HTML, CSS, JavaScript and I have experience working with CCS
                   libraries such as Materialize and Bootstrap.
-                </span>{" "}
-                I am also vast in JavaScript based frameworks such as:{" "}
+                </span>
+                I am also vast in JavaScript based frameworks such as:
                 <span>jQuery, Node JS, React JS, Express JS and so on....</span>
               </h4>
-              <br />
               <br />
               <br />
               <br />
